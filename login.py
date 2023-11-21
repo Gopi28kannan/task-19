5d3df4cd13cd61c63a2a335a228cb2e6a1b95092
Gopi28kannan <144014765+Gopi28kannan@users.noreply.github.com>
2023-11-21 22:03:24 +0530

    Add files via upload

---
+++ login.py
@@ -0,0 +1,47 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from time import sleep
+from selenium.common.exceptions import NoSuchElementException
+class webpage:
+    username = "problem_user"
+    password = "secret_sauce"
+
+    def __init__(self,web_url):
+        self.url = web_url
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+    def logging(self):
+        try:
+            self.driver.maximize_window()
+            self.driver.get(self.url)
+            sleep(1)
+            self.driver.find_element(By.NAME, value="user-name").send_keys(self.username)
+            sleep(4)
+            self.driver.find_element(By.NAME, value="password").send_keys(self.password)
+            sleep(4)
+            self.driver.find_element(By.XPATH, "//input[@id='login-button']").click()
+        except NoSuchElementException as selenium_error:
+            print(selenium_error)
+
+    def data_fetch(self):
+        driver = self.driver
+        webpage_title = driver.title
+        current_url = driver.current_url
+        webpage_content = driver.page_source
+        with open("webpage_task_11.txt", "w", encoding="utf-8") as file:
+            file.write(webpage_content)
+        return webpage_title, current_url
+    def sh(self):
+        sleep(10)
+        self.driver.quit()
+        
+url = "https://www.saucedemo.com/"
+wb = webpage(url)
+wb.logging()
+title, current_url = wb.data_fetch()
+print("Webpage Title  : ", title)
+print("Current URL     : ", current_url)
+print("Webpage content saved to 'webpage_task_11.txt'")
+wb.sh()
